c577462b99e91e9d16304f1dad4cb4c738172015
daniela Duque <danduquegar@unal.edu.co>
2020-10-17 16:27:18 -0500

    :sparkles: rula class

--- Analizador_sintactico/sintasis/rule.py
+++ Analizador_sintactico/sintasis/rule.py
@@ -1,15 +1,15 @@
 
 
 class Rule ():
-    def __init__ (self, rigth_part, predition_st = []):
+    def __init__ (self, rigth_part, prediction_set = []):
         self.rigth_part = rigth_part # Array de string
-        self.predition_st = predition_st
+        self.prediction_set = prediction_set
 
     def set_prediction_set(self,predition_st):
         self.predition_st = predition_st
 
     def put_prediction_set (self, terminal):
-        self.prediction_st.append(terminal)
+        self.prediction_set.append(terminal)
     
     def __str__(self):
         return  ",".join(self.rigth_part)
